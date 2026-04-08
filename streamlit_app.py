5f65c7474870c4df130d1dfcab3c87f624f84990
ChickenMan-1080 <justletmein.comonman@gmail.com>
2026-04-09 03:51:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('🤖 Machine Learning App')
 
-st.markdown('## Hi This App is for study about Machine learning for me!')
+st.markdown('## Hi! This App is for study about Machine learning for me!')
