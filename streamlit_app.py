4e8f4b381d44caeed78537702c2a83edd5108639
Phee <pheerapol.phee1080@gmail.com>
2026-04-09 05:10:51 +0700

    updated : expander

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,7 @@ st.title('🤖 Machine Learning App')
 
 st.markdown('## Hi! This App is for study about Machine Learning and WEB APP for me!')
 
-df = pd.read_csv("assets/penguins_cleaned.csv")
-df
+with st.expander('Data'):
+    st.write('Raw Data')
+    df = pd.read_csv("assets/penguins_cleaned.csv")
+    df
