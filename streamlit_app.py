4405ebb8145d28ec40223100647b8eab16adcbf7
Phee <pheerapol.phee1080@gmail.com>
2026-04-09 05:05:52 +0700

    updated : dataset

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
+import os
 
 st.title('🤖 Machine Learning App')
 
 st.markdown('## Hi! This App is for study about Machine Learning and WEB APP for me!')
+
+df = pd.read_csv("assets/penguins_cleaned.csv")
+df
